1c5374b0f736bbbbee4adf602bc44d28d77b8e07
Farokh Madah <fa.madah@gmail.com>
2024-10-31 19:04:47 +0330

    ui

--- app/(auth)/Header.tsx
+++ app/(auth)/Header.tsx
@@ -1,19 +1,23 @@
-import { Button } from '@/components/ui/button'
-import Link from "next/link";
+'use client'
 
-export default function Header() {
+const Header = () => {
   return (
-    <header className='p-4'>
-      <nav className='max-w-6xl mx-auto flex items-center justify-between'>
-        <div className="flex gap-5 item-center">
-          <Link href="https://www.tezuka.xyz" className="flex items-center space-x-1 rtl:space-x-reverse">
-            <img src="/logos/logo-wide.webp" className="h-8" alt="Tezuka Logo" />
-          </Link>
-        </div>
-        <div className='flex items-center text-green-500'>
-          Artist Login
+    <section className="antialiased w-full" id="Header">
+      <nav className="w-full top-0 left-0 px-2 bg-white border-gray-200 dark:bg-black/90 z-50">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="https://tezuka.xyz" className="flex items-center space-x-1 rtl:space-x-reverse">
+            <img src="/logos/logo.webp" className="md:hidden h-12" alt="Tezuka Logo" />
+            <img src="/logos/logo-wide.webp" className="hidden md:block h-12" alt="Tezuka Logo" />
+          </a>
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <p className="text-green-1 px-4 py-2 text-center text-body-semibold">
+              Artist Login
+            </p>
+          </div>
         </div>
       </nav>
-    </header>
+    </section>
   )
 }
+
+export default Header
