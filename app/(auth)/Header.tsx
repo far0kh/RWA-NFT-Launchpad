679fd33e13f0423617151f0b39f7dc6d116ffd29
Farokh Madah <fa.madah@gmail.com>
2024-11-04 14:00:03 +0330

    site url

--- app/(auth)/Header.tsx
+++ app/(auth)/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <section className="antialiased w-full" id="Header">
       <nav className="w-full top-0 left-0 px-2 bg-white border-gray-200 dark:bg-black/90 z-50">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://tezuka.xyz" className="flex items-center space-x-1 rtl:space-x-reverse">
+          <a href="https://www.tezuka.xyz" className="flex items-center space-x-1 rtl:space-x-reverse">
             <img src="/logos/logo.webp" className="md:hidden h-12" alt="Tezuka Logo" />
             <img src="/logos/logo-wide.webp" className="hidden md:block h-12" alt="Tezuka Logo" />
           </a>
