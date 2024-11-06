c11779a035f066bccee44e5e35d950d70710203b
Farokh Madah <fa.madah@gmail.com>
2024-11-07 03:06:42 +0330

    check if user exist

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -5,7 +5,7 @@ export const createUser = async (user: UserType) => {
   try {
     await connectToDB();
     const existingUser = await User.findOne({ email_address: user.email_address })
-    if (existingUser.clerk_id) {
+    if (existingUser?.clerk_id) {
       return JSON.parse(JSON.stringify(existingUser));
     }
     const newUser = await User.create(user);
