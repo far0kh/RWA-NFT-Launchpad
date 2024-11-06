60064970e9892783eeff42899d437bce122b10db
Farokh Madah <fa.madah@gmail.com>
2024-11-07 03:19:13 +0330

    create user

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -4,10 +4,10 @@ import { connectToDB } from "../mongoDB"
 export const createUser = async (user: UserType) => {
   try {
     await connectToDB();
-    const existingUser = await User.findOne({ email_address: user.email_address })
-    if (existingUser?.clerk_id) {
-      return JSON.parse(JSON.stringify(existingUser));
-    }
+    // const existingUser = await User.findOne({ email_address: user.email_address })
+    // if (existingUser?.clerk_id) {
+    //   return JSON.parse(JSON.stringify(existingUser));
+    // }
     const newUser = await User.create(user);
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
